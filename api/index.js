fa0a46a1ed1ac07e4689160e7a0e52ec92f789b5
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-03-29 09:50:32 -0300

    Creo que esta listo

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
+const expressApp = require("../public/express/express.js" ); // Importar la aplicación Express configurada en express.js
+app.use(expressApp);
+
+//app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
+
+app.get("/datos", (req, res) => res.sendFile("public/cliente_servidor/datos.json"));
+
+app.get("/cliente_servidor", (req, res) => res.sendFile("public/cliente_servidor/index.html"));
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
---
+++ public/express/express.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+module.exports = app;
+
+const datos = [  {
+    "email": "usuario1@example.com",
+    "contrasena": "contrasena123",
+    "genero": "Femenino"
+  },
+  {
+    "email": "usuario2@example.com",
+    "contrasena": "contrasena456",
+    "genero": "Masculino"
+  },
+  {
+    "email": "usuario3@example.com",
+    "contrasena": "contrasena789",
+    "genero": "No especificado"
+  }];
+
+  const fs = require("fs");
+  app.get("/express", (req, res) => {
+    let index = `
+      <h1>Usuarios</h1>
+      <table border="1">
+        <tr>
+          <th>Email</th>
+          <th>Contraseña</th>
+          <th>Género</th>
+        </tr>`;
+  
+    // Añadir cada usuario a la tabla
+    datos.forEach(usuario => {
+      index += `
+        <tr>
+          <td>${usuario.email}</td>
+          <td>${usuario.contrasena}</td>
+          <td>${usuario.genero}</td>
+        </tr>`;
+    });
+  
+    index += `</table>`;
+
+    
+    // Leer el archivo index.html y agregar la tabla
+    fs.readFile("public/express/index.html", "utf8", (err, html) => {
+        if (err) {
+            console.error("Error al leer el archivo index.html:", err);
+            res.status(500).send("Error interno del servidor");
+            return;
+        }
+
+         // Insertar la tabla en el HTML
+        const modifiedHTML = html.replace("<!-- TablaDatos -->", index);
+
+        // Enviar la respuesta con el HTML modificado
+        res.send(modifiedHTML);
+    });
+});
