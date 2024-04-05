042003551e63efd002c85d34eb80498d4c9ef3a3
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-04-05 14:39:21 -0300

    efrtgh

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 
-const expressApp = require("../src/express.js" ); // Importar la aplicación Express configurada en express.js
+//const expressApp = require("../src/express.js" ); // Importar la aplicación Express configurada en express.js
+const expressApp = require(path.resolve(_dirname, '../src/express.js'));
 app.use(expressApp);
 
 //app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
 
-app.get("/datos", (req, res) => res.sendFile("public/cliente_servidor/datos.json"));
+app.get("/cliente_servidor", (req, res) => res.sendFile("public/cliente_servidor/index.html", {root: '.'}));
 
-app.get("/cliente_servidor", (req, res) => res.sendFile("public/cliente_servidor/index.html"));
+app.get("/datos", (req, res) => res.sendFile(path.sendFile(_dirname,  '../public/cliente_servidor/datos.json')));
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
